7f9e3ada72bb2f768369ee977416d9961491ae24
HieuVu <hieuvu.cdt95@gmail.com>
2024-03-30 18:58:36 +0700

    finish

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/handling/ExceptionHandling.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/handling/ExceptionHandling.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.handling;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
@@ -12,5 +13,9 @@ public class ExceptionHandling {
     {
         redirectAttributes.addFlashAttribute("error", "File size exceeds allowable limit.");
         return "redirect:/result?error";
+    }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleBadUrl(Exception ex) {
+        return "redirect:/login";
     }
 }
